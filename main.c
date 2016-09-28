afbf633d67f3a9bf3597bef6f75477016d4b0522
Bijan Tabatabai <bijan311@yahoo.com>
2016-09-27 19:29:42 -0500

    Added unit unit tests

--- main.c
+++ main.c
@@ -1,25 +1,71 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdbool.h>
 #include "CircularQueue.h"
+
+int testNum = 1;
+
+void assert(int comparison)
+{
+	printf("Test #%d ", testNum);
+	if(comparison == false)
+	{
+		printf("has failed\n");
+	}
+	else
+	{
+		printf("has passed\n");
+	}
+
+	testNum++;
+}
 
 int main(void)
 {
 	int array[5];
 	CircularQueue queue;
 	int array2[5] = {1, 2, 3, 4, 5};	
+	
 	assert(CQ_Init(&queue, (void*)array, sizeof(int), 5) == 1);
 
 	for(int i = 0; i < 5; i++)
 	{
 		CQ_Enqueue(&queue, &array2[i]);
 	}
+	
+	//Test that the queue does not allow having more elements than there is capacity
+	assert(CQ_Enqueue(&queue, &array2[3]) == 0);
 
 	for(int i = 0; i < 5; i++)
 	{
 		int number = 1;
 		CQ_Dequeue(&queue, &number);
-		printf("%d\n", number);
+		//printf("%d\n", number);
 	}
 
+	//Test that the queue reports a failure when trying to dequeue when there are no elements in the queue
+	int i;
+	assert(CQ_Dequeue(&queue, &i) == 0);
+
+	//Test queueing chars
+	CircularQueue charQueue;
+	char emptyCharArray[13];
+	char* helloStr = "Hello World!";
+	char recievingArray[13];
+
+	CQ_Init(&charQueue, (void*)emptyCharArray, sizeof(char), 13);
+
+	for(int i = 0; i < 13; i++)
+	{
+		CQ_Enqueue(&charQueue, &helloStr[i]);
+	}
+	for(int i = 0; i < 13; i++)
+	{
+		char input = 's';
+		CQ_Dequeue(&charQueue, &input);
+		recievingArray[i] = input;
+	}
+
+	assert(strcmp(helloStr, recievingArray) == 0);
+	
 	return 0;
 }
