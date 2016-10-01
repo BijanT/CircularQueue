ddcc1b883b6a6a0f21f2f7126f500436611388b8
type0show <ychen486@wisc.edu>
2016-10-01 10:25:28 -0500

    Added data member "freeBytes"
    "freeBytes" will be used to indicate the available space in the queue. It will be updated every time any enqueue/dequeue function is called.

--- CircularQueue.h
+++ CircularQueue.h
@@ -11,8 +11,10 @@ typedef struct CircularQueue
 	int back;
 	int itemSize;
 	int capacity;//The max number of elements that can be stored in the array 
-	int arraySize;//The size of the array in bites
+	int arraySize;//The size of the array in bytes
 	int numElements;
+	uint32_t freeBytes;//The total number of free bytes in the array
+	
 }CircularQueue;
 
 int CQ_Enqueue(CircularQueue* queue, void* itemPtr);
