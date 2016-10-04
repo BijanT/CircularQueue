c78df4c2cb5be3dc3fea79dd5e83ff20214bce23
Bijan Tabatabai <bijan311@yahoo.com>
2016-10-03 19:05:36 -0500

    Changed data tyoes in CircularQueue struct from int to unsigned int

--- CircularQueue.h
+++ CircularQueue.h
@@ -7,13 +7,13 @@
 typedef struct CircularQueue
 {
 	void* arrayPtr;
-	int front;
-	int back;
-	int itemSize;
-	int capacity;//The max number of elements that can be stored in the array 
-	int arraySize;//The size of the array in bytes
-	int numElements;
-	uint32_t freeBytes;//The total number of free bytes in the array
+	unsigned int front;
+	unsigned int back;
+	unsigned int itemSize;
+	unsigned int capacity;//The max number of elements that can be stored in the array 
+	unsigned int arraySize;//The size of the array in bytes
+	unsigned int numElements;
+	unsigned int freeBytes;//The total number of free bytes in the array
 	
 }CircularQueue;
 
